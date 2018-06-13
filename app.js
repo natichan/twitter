d2f99d53808c5e027fa92551be0109dfa73646b2
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-06-13 01:28:09 -0400

    Agregue condición para que el botón no se ejecute si es un mensaje vacio

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const boton= document.getElementById("btn"); //Llamamos al botón.
-//Dato "onclick" solo va en HTML, se ejecuta solo en el HTML.
-//Evento addEventListener escucha cuando se ejecuta el click y acciona.
-boton.addEventListener("click", () => {
-    //el lugar para alojar el mensaje
-    let comments = document.getElementById("comment").value;//rescato el mensaje
-    //limpiar la caja si hay algo escrito con el siguiente comando:
-    document.getElementById("comment").value = "";
-    //creando variable con el contenedor que rescatara el mensaje(guardara)
-    const cont = document.getElementById("cont");
-    //creando un div con creatElement y entre parentesis lo que queremos crear
-    const newComments = document.createElement("div");
-    const message = document.createElement("text");
-    message.type = "text";
-    const contenedorElemento = document.createElement("p");
-    let textNewComment = document.createTextNode(comments)//le decimos que es un texto y lo alojamos en el comentario
-    contenedorElemento.appendChild(textNewComment);//lo asignamos a su padre párrafo
-    //le asignamos todos los padres a sus hijos
-    newComments.appendChild(message);
-    newComments.appendChild(contenedorElemento);
-    cont.appendChild(newComments);
+//Si no escriben en el id comment (donde va el mensaje), entonces que no se ejecute la acción event listener
+
+boton.addEventListener("click", (e) => {
+    //compruebo si el campo esta vacio 
+    //si está vacio prevenir que suceda el evento
+    if (comment.value === '') {
+        e.preventDefault();
+      }else{
+        //el lugar para alojar el mensaje
+        let comments = document.getElementById("comment").value;//llamo al mensaje
+        //limpiar la caja si hay algo escrito con el siguiente comando:
+        document.getElementById("comment").value = "";
+        //creando variable con el contenedor que rescatara el mensaje(guardara)
+        const cont = document.getElementById("cont");
+        //creando un div con creatElement y entre parentesis lo que queremos crear
+        const newComments = document.createElement("div");
+        //LMS: Crea un nodo de tipo Element que representa al elemento XHTML cuya etiqueta se pasa como parámetro
+        const message = document.createElement("text");
+        message.type = "text";
+        const contenedorElemento = document.createElement("p");
+        //LMS: Crea un nodo de tipo Text que almacena el contenido textual de los elementos XHTML
+        let textNewComment = document.createTextNode(comments)
+        //añadimos el nodo textNewComment como hijo del nodo Element
+        contenedorElemento.appendChild(textNewComment);//lo asignamos a su padre párrafo
+        //le asignamos el padre a sus hijos
+        newComments.appendChild(message);
+        newComments.appendChild(contenedorElemento);
+        cont.appendChild(newComments);
+    }
 }) 
